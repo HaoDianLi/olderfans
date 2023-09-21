78766cb75c7b815874f385fa313475a0d19aac85
Hao Dian Li <hli144@stevens.edu>
2023-09-21 02:48:12 -0400

    validated most of create() fn except birthdate

--- data/fans.js
+++ data/fans.js
@@ -1,5 +1,6 @@
 import { ObjectId } from "mongodb";
 import { fans, posts } from "../config/mongoCollections.js";
+import helpers from "../helpers/validation.js";
 import bcrypt from 'bcrypt'
 const saltRounds = 12;
 import fanData from "./fans.js";
@@ -13,21 +14,68 @@ const exportedMethods = {
         email,
         birthDate,
         password
-    ){
+    ) {
+
+        // throw error if any field is empty
         if (!firstName || !lastName || !username || !email || !birthDate || !password) {
             throw new Error("Error: All of the fields must be filled.");
         }
-          
-        if (typeof firstName !== "string") throw `Error: First name must be a string type value.`;
-        if (typeof lastName !== "string") throw `Error: Last name must be a string type value.`;
-        if (typeof username !== "string") throw `Error: Username name must be a string type value.`;
+        
+        // throw error if names are not strings
+        if (typeof firstName !== "string") throw new Error("First name must be a string type value.");
+        if (typeof lastName !== "string") throw new Error("Last name must be a string type value.");
+        if (typeof username !== "string") throw new Error("Username name must be a string type value.");
 
+        // remove extra spaces
         firstName = firstName.trim();
         lastName = lastName.trim();
         username = username.trim();
-        birthDate = birthDate.trim();
+        email = email.trim();
 
+        // reference fans collection
         const fansCollection = await fans();
+
+                                        // validate field inputs
+
+        // convert names lowercase
+        const firstNameLower = helpers.convertLowercase(firstName);
+        const lastNameLower = helpers.convertLowercase(lastName);
+        const usernameLower = helpers.convertLowercase(username);
+
+        // check names chars
+        if (!helpers.onlyLetters(firstNameLower)) throw new Error("Invalid first name character (only letters allowed).");
+        if (!helpers.onlyLetters(lastNameLower)) throw new Error("Invalid last name character (only letters allowed).");
+        if (!helpers.allowedChars(username)) throw new Error("Invalid username character (only letters, numbers, underscores, hyphens, and periods allowed).");
+
+        // check names lengths 25
+        if (helpers.countChars(firstName) > 25) throw new Error("First name too long (max: 25 characters).");
+        if (helpers.countChars(lastName) > 25) throw new Error("Last name too long (max: 25 characters).");
+        if (helpers.countChars(username) > 25) throw new Error("Username too long (max: 25 characters).");
+
+        // check username dup
+        const existingUsername = await fansCollection.findOne({ username: usernameLower });
+        if (existingUsername) throw new Error("Username already exists.");
+        
+        // convert email lowercase
+        const emailLower = helpers.convertLowercase(email);
+
+        const emailParts = helpers.tokenizeEmailAt(email);
+        // only one @ in email
+        if (!emailParts) throw new Error("Error: Invalid email format.");
+
+        // email domain vibe check
+        if (!helpers.domainRestrictions(emailParts.domainPart)) throw "Error: Invalid email format.";
+        
+        // check email length 50
+        if (helpers.countChars(email) > 50) throw new Error("First name too long (max: 50 characters).");
+        
+        // check email dup
+        const existingEmail = await fansCollection.findOne({ email: emailLower });
+        if (existingEmail) throw new Error("Email is already associated with another account.");
+
+                                        // validate birthdate
+
+        birthDate = birthDate.trim();       // TODO
         
         const hashedPassword = await bcrypt.hash(password, saltRounds);
 
@@ -79,15 +127,15 @@ const exportedMethods = {
 
 };
 
-// const testFan = await exportedMethods.create(
-//     "Cidolfus",
-//     "Telamon",
-//     "ramuh",
-//     "cid@ffxvi.com",
-//     "2000-01-01",
-//     "testpassword"
-// );
+const testFan = await exportedMethods.create(
+    "Cidolfus",
+    "Telamon",
+    "ram.uh",
+    "cid@ffxvi.com",
+    "2000-01-01",
+    "testpassword"
+);
 
-// console.log("Test Fan Created:", testFan);
+console.log("Test Fan Created:", testFan);
 
 export default exportedMethods;
---
+++ helpers/validation.js
@@ -0,0 +1,78 @@
+
+const helpers = {
+    
+    countChars(string) {
+
+        let count = 0;
+        for (let c in string) count++;
+        return count;
+
+    },
+
+    convertLowercase(string) { return string.toLowerCase(); },
+
+    onlyLetters(string) {
+
+        const pattern = /^[a-zA-Z]+$/;
+        if (pattern.test(string)) return true;
+        else return false;
+
+    },
+
+    allowedChars(string) {
+
+        const pattern = /^[a-zA-Z0-9_\-\.]+$/;
+        if (pattern.test(string)) return true;
+        else return false;
+
+    },
+
+    tokenizeEmailAt(email) {
+
+        const parts = email.split('@');
+
+        if (parts.length !== 2) return null;
+
+        const localPart = parts[0];
+        const domainPart = parts[1];
+
+        return { localPart, domainPart };
+
+    },
+
+    domainRestrictions(domain) {
+
+        const validTLDs = [
+            "com",
+            "org",
+            "net",
+            "gov",
+        ];
+
+        const parts = domain.split('.');
+        const len = parts.length;
+
+        // checks for at least one period
+        if (len === 1) return null;
+
+        // check for valid tld
+        const tld = parts[len - 1].toLowerCase();
+        if (!validTLDs.includes(tld)) return null;
+
+        // cannot start with period
+        if (parts[0] === '.') return null;
+
+        for (let i = 0; i < len; i++) {
+            // check for consecutive periods
+            if (parts[i].length === 0) return null;
+            // check for special chars
+            if (!helpers.onlyLetters(parts[i])) return null;
+        }
+
+        return tld;
+
+    }
+
+};
+
+export default helpers;
