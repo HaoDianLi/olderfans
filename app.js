c0e0e80fda71bd0920774ae2724e9da98bef81da
Hao Dian Li <hli144@stevens.edu>
2023-09-25 23:49:20 -0400

    added side nav and started on routes

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
 import express from 'express';
-import exphbs from 'express-handlebars';
+const app = express();
+import configRoutes from './routes/index.js';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-
-import configRoutes from './routes/index.js';
-
-const app = express();
-
-// set handlebars as the template engine
-app.engine('handlebars', exphbs.engine());
-app.set('view engine', 'handlebars'); // set view engine to handlebars
+import exphbs from 'express-handlebars';
+import fansRouter from './routes/fans.js';
+import postsRouter from './routes/posts.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 const staticDir = express.static(__dirname + '/public');
 
 app.use('/public', staticDir);
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/', fansRouter);
+app.use('/', postsRouter);
 
-// middleware and route configs
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
 
 // updates currentYear in main 
 app.use((req, res, next) => {
   res.locals.currentYear = new Date().getFullYear();
   next();
+});
+
+app.get('/', (req, res) => {
+  res.render('homepage');
 });
 
 configRoutes(app);
--- routes/fans.js
+++ routes/fans.js
@@ -2,9 +2,36 @@ import { ObjectId } from 'mongodb';
 import express from 'express';
 const router = express.Router();
 import axios from 'axios';
-import xss from 'xss';
 
-router
-    .get('/');
+router.route('/').get(async (req, res) => {
+    try {
+        res.render('homepage');
+    } catch (error) {
+        res.render('error', { error });
+    }
+});
+
+router.route('/about').get(async (req, res) => {
+    try {
+        res.render('about');
+    } 
+    catch (error) {
+        res.render('error', { error });
+    }
+});
+
+router.route('/contact').get(async (req, res) => {
+    try {
+        res.render('contact');
+    } 
+    catch (error) {
+        res.render('error', { error });
+    }
+});
+
+router.get('/signout', async (req, res) => {
+    req.session.destroy();
+    res.redirect("/");
+});
 
 export default router;
